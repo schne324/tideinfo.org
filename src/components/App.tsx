cbc32ed9d41d8233c4bdc2ea44761c09ae52b343
Harris Schneiderman <schne324@gmail.com>
2020-07-19 22:29:04 -0700

    stuff/things/such/etc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,43 +6,115 @@ interface TideEntry {
   type: string;
 }
 
+interface Metadata {
+  id: string;
+  name: string;
+  lat: string;
+  lon: string;
+}
+
+interface TempData {
+  t: string;
+  v: string;
+  f: string;
+}
+
+interface WaterTemp {
+  metadata: Metadata;
+  data: TempData[];
+}
+
 interface Props {
   stationValue: string;
   onStationChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   tideData: TideEntry[];
+  error?: string;
+  temperatureData?: WaterTemp;
 }
+
+interface TideDateData {
+  level: string;
+  type: string;
+}
+
+interface DateData {
+  [key: string]: TideDateData;
+}
+
+interface DataByDate {
+  [key: string]: DateData;
+}
+
+const get12HourTime = (t: string) => {
+  const [hours, mins] = t.split(':');
+  const convertedHours = ((parseInt(hours) + 1) % 12) + 1;
+  const amPm = convertedHours > 11 ? 'pm' : 'am';
+  return `${hours}:${mins}${amPm}`;
+};
 
 const App: React.ComponentType<Props> = ({
   onStationChange,
   stationValue,
   onSubmit,
   tideData,
-}) => (
-  <main>
-    <h1>
-      <span>Tide</span> <span>Info</span>
-    </h1>
-    <form onSubmit={onSubmit}>
-      <label htmlFor="station">Station</label>
-      <input
-        type="text"
-        onChange={onStationChange}
-        id="station"
-        value={stationValue}
-      />
-      <button type="submit">Submit</button>
-    </form>
-    {!!tideData.length && (
-      <ul>
-        {tideData.map((data) => (
-          <li key={data.t}>
-            {data.t} / {data.v} / {data.type}
-          </li>
-        ))}
-      </ul>
-    )}
-  </main>
-);
+  error,
+  temperatureData,
+}) => {
+  const tideDataByDate =
+    !!tideData &&
+    !!tideData.length &&
+    tideData.reduce((dataByDate: DataByDate, { t, v, type }) => {
+      const [date, time] = t.split(' ');
+      dataByDate[date] = dataByDate[date] || {};
+      dataByDate[date][time] = { level: v, type };
+      return dataByDate;
+    }, {});
+
+  return (
+    <main>
+      <h1>
+        <span>Tide</span> <span>Info</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="station">Station</label>
+        <input
+          type="text"
+          onChange={onStationChange}
+          id="station"
+          value={stationValue}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {!!temperatureData && (
+        <>
+          <h2>{temperatureData.metadata.name}</h2>
+          <p>Water temperature: {temperatureData.data[0].v}</p>
+        </>
+      )}
+      {tideDataByDate && (
+        <ul>
+          {Object.entries(tideDataByDate).map(([date, data]) => {
+            return (
+              <li key={date}>
+                <div>{date}</div>
+                <ul>
+                  {Object.entries(data).map(([time, tideInfo]) => (
+                    <li key={`${date}${time}`}>
+                      <div>{tideInfo.type === 'H' ? 'High' : 'Low'}</div>
+                      <div>{get12HourTime(time)}</div>
+                      <div>{tideInfo.level}</div>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {error && <p>Some shit went wrong: {error}</p>}
+    </main>
+  );
+};
 
 export default App;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,30 +3,78 @@ import queryString from 'query-string';
 import App from '../components/App';
 
 const BASE_URL = 'https://tidesandcurrents.noaa.gov/api/datagetter';
+const zeroIfy = (n: number) => (`${n}`.length === 2 ? n : `0${n}`);
+const d = new Date();
+const tomorrow = new Date(d);
+tomorrow.setDate(tomorrow.getDate() + 1);
+const defaultParams = {
+  station: '9410170', // San Diego Bay
+  datum: 'MLLW',
+  units: 'english',
+  time_zone: 'LST_LDT',
+  format: 'json',
+};
+const tideParams = {
+  ...defaultParams,
+  product: 'predictions',
+  format: 'json',
+  application: 'tideinfo', // NOS.COOPS.TAC.COOPSMAP
+  interval: 'hilo',
+  begin_date: `${d.getFullYear()}${zeroIfy(d.getMonth())}${zeroIfy(
+    d.getDate()
+  )}`,
+  end_date: `${tomorrow.getFullYear()}${zeroIfy(tomorrow.getMonth())}${zeroIfy(
+    tomorrow.getDate()
+  )}`,
+};
+const tempParams = {
+  ...defaultParams,
+  station: '9410170',
+  date: 'latest',
+  product: 'water_temperature',
+  application: 'tideinfo',
+  interval: '',
+};
 
 const AppContainer: React.ComponentType = () => {
-  const [stationValue, setStationValue] = useState('');
-  const [station, setStation] = useState('');
+  const [stationValue, setStationValue] = useState(tideParams.station);
+  const [error, setError] = useState();
   const [tideData, setTideData] = useState([]);
+  const [temperatureData, setTemperatureData] = useState();
   const onStationChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setStationValue(e.target.value);
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setStation(stationValue);
-    const res = await fetch(
-      `${BASE_URL}?date=today&station=9410170&product=predictions&interval=hilo&units=english&datum=STND&time_zone=lst&format=json`
-    );
-    const json = await res.json();
-    console.log('JSON: ', json);
-    setTideData(json.predictions);
-  };
 
+    // fetch the tide data
+    const tideRes = await fetch(
+      `${BASE_URL}?${queryString.stringify({
+        ...tideParams,
+        station: stationValue,
+      })}`
+    );
+    const tideData = await tideRes.json();
+    setTideData(tideData.predictions);
+    setError(tideData.error ? tideData.error.message : '');
+
+    // fetch the water temperature data
+    const waterTempRes = await fetch(
+      `${BASE_URL}?${queryString.stringify({
+        ...tempParams,
+        station: stationValue,
+      })}`
+    );
+    setTemperatureData(await waterTempRes.json());
+  };
+  console.log({ temperatureData });
   return (
     <App
       onStationChange={onStationChange}
       stationValue={stationValue}
       onSubmit={onSubmit}
       tideData={tideData}
+      error={error}
+      temperatureData={temperatureData}
     />
   );
 };
